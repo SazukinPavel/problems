8f62355b261c4f926e3894c69aed9f537a96ee30
SazukinPavel <sazukinpavelal@gmail.com>
2023-03-26 17:40:00 +0300

    add generics

--- list/list.cpp
+++ list/list.cpp
@@ -19,12 +19,22 @@ private:
   }
 
 public:
-  List(){};
-  List(int capasity) { this->capacity = capasity; };
+  List() {
+    root = nullptr;
+    last = nullptr;
+  };
+  List(int capasity) {
+    this->capacity = capasity;
+    root = nullptr;
+    last = nullptr;
+  };
+  ~List() {
+    delete root;
+    delete last;
+  };
   int count() { return this->nodesCount; }
   Node<T> *getRoot() { return root; };
   void push(T data) {
-    cout << data << root << last;
     if (this->count() > capacity) {
       throw "Capacity error";
     }
@@ -102,41 +112,40 @@ public:
 
 int main() {
   try {
-    //    cout << "LIST TYPE INTEGER"
-    //         << "\n";
-    //    List<int> *list = new List<int>(10);
-    //    cout << " is empty " << list->is_empty() << "\n";
-    //    list->push(1);
-    //    list->push(2);
-    //    list->push(3);
-    //    list->push(4);
-    //    list->push(5);
-    //    list->push(6);
-    //    list->unshift(7);
-    //    cout << " count " << list->count() << "\n";
-    //    cout << " pop " << list->pop() << "\n";
-    //    cout << " shift " << list->shift() << "\n";
-    //    cout << " count " << list->count() << "\n";
-    //    cout << " find 4 " << list->find(4) << "\n";
-    //    cout << " find 666 " << list->find(666) << "\n";
-    //    delete list;
-    //    cout << "LIST TYPE STRING"
-    //         << "\n";
-    List<std::string> *list2 = new List<std::string>(10);
-    cout << " is empty " << list2->is_empty() << "\n";
-    list2->push("some 1");
-    list2->push("some 2");
-    list2->push("some 3");
-    list2->push("some 4");
-    list2->push("some 5");
-    list2->push("some 6");
-    list2->unshift("some 7");
-    cout << " count " << list2->count() << "\n";
-    cout << " pop " << list2->pop() << "\n";
-    cout << " shift " << list2->shift() << "\n";
-    cout << " count " << list2->count() << "\n";
-    cout << " find some 4 " << list2->find("some 4") << "\n";
-    cout << " find some666 " << list2->find("some666") << "\n";
+    cout << "LIST TYPE INTEGER"
+         << "\n";
+    List<int> list(10);
+    cout << " is empty " << list.is_empty() << "\n";
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    list.push(4);
+    list.push(5);
+    list.push(6);
+    list.unshift(7);
+    cout << " count " << list.count() << "\n";
+    cout << " pop " << list.pop() << "\n";
+    cout << " shift " << list.shift() << "\n";
+    cout << " count " << list.count() << "\n";
+    cout << " find 4 " << list.find(4) << "\n";
+    cout << " find 666 " << list.find(666) << "\n";
+    cout << "LIST TYPE STRING"
+         << "\n";
+    List<std::string> list2(10);
+    cout << " is empty " << list2.is_empty() << "\n";
+    list2.push("some 1");
+    list2.push("some 2");
+    list2.push("some 3");
+    list2.push("some 4");
+    list2.push("some 5");
+    list2.push("some 6");
+    list2.unshift("some 7");
+    cout << " count " << list2.count() << "\n";
+    cout << " pop " << list2.pop() << "\n";
+    cout << " shift " << list2.shift() << "\n";
+    cout << " count " << list2.count() << "\n";
+    cout << " find some 4 " << list2.find("some 4") << "\n";
+    cout << " find some666 " << list2.find("some666") << "\n";
   } catch (const char *error) {
     cout << "External execption: " << error << std::endl;
   }
