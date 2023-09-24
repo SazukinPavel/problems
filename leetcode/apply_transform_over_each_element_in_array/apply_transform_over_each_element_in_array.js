61f9bbbceac9c9e22cb9c0ee6674a44c8a4fcc0a
SazukinPavel <sazukinpavelal@gmail.com>
2023-09-24 17:33:26 +0400

    add apply_transform_over_each_element_in_array

---
+++ leetcode/apply_transform_over_each_element_in_array/apply_transform_over_each_element_in_array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+  const result = [];
+
+  for (let index = 0; index < arr.length; index++) {
+    result.push(fn(arr[index], index, arr));
+  }
+
+  return result;
+};
